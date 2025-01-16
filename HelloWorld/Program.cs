c08a8b8b3ab287674cfd13a0a499f5681d8931d7
GuilhermeFrancisco7 <guilherme.falmeida7@senacsp.edu.sp>
2025-01-16 15:19:30 -0300

    Hello World

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Globalization;
+
+namespace HelloWorld
+
+{
+    class Program
+    {
+
+        enum DiasSemana {Domingo,Segunda,Terça,Quarta,Quinta,Sexta,Sábado};
+       public static void Main(string[] args)
+
+        {
+            
+            int i =  1000000000;
+            short s = (short)i;
+            
+
+            Console.WriteLine(s);
+        }
+           
+    }
+
+    
+}
+
+
+
+
