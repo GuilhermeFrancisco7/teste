5393391cfeb67771de15f71a760cab68cf4b0ecf
Guilherme Francisco Almeida <guilhermefranciscoalmeida22@gmail.com>
2025-03-25 15:09:00 -0300

    switch

---
+++ switch-case/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace switch_case
+{
+    partial class frmswitch
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbltitulo = new System.Windows.Forms.Label();
+            this.txtCodeInserido = new System.Windows.Forms.TextBox();
+            this.lblDescricao = new System.Windows.Forms.Label();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbltitulo
+            // 
+            this.lbltitulo.AutoSize = true;
+            this.lbltitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitulo.ForeColor = System.Drawing.Color.Chocolate;
+            this.lbltitulo.Location = new System.Drawing.Point(267, 66);
+            this.lbltitulo.Name = "lbltitulo";
+            this.lbltitulo.Size = new System.Drawing.Size(263, 25);
+            this.lbltitulo.TabIndex = 0;
+            this.lbltitulo.Text = "Insira um Código de 1 a 6 ";
+            // 
+            // txtCodeInserido
+            // 
+            this.txtCodeInserido.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodeInserido.Location = new System.Drawing.Point(335, 121);
+            this.txtCodeInserido.Name = "txtCodeInserido";
+            this.txtCodeInserido.Size = new System.Drawing.Size(126, 26);
+            this.txtCodeInserido.TabIndex = 1;
+            this.txtCodeInserido.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblDescricao
+            // 
+            this.lblDescricao.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDescricao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescricao.Location = new System.Drawing.Point(335, 175);
+            this.lblDescricao.Name = "lblDescricao";
+            this.lblDescricao.Size = new System.Drawing.Size(126, 22);
+            this.lblDescricao.TabIndex = 2;
+            this.lblDescricao.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.BackColor = System.Drawing.Color.Black;
+            this.btnEnviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.ForeColor = System.Drawing.Color.Gold;
+            this.btnEnviar.Location = new System.Drawing.Point(327, 257);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(142, 62);
+            this.btnEnviar.TabIndex = 3;
+            this.btnEnviar.Text = "Enviar Código";
+            this.btnEnviar.UseVisualStyleBackColor = false;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // frmswitch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.lblDescricao);
+            this.Controls.Add(this.txtCodeInserido);
+            this.Controls.Add(this.lbltitulo);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmswitch";
+            this.Text = "Condicional Switch";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbltitulo;
+        private System.Windows.Forms.TextBox txtCodeInserido;
+        private System.Windows.Forms.Label lblDescricao;
+        private System.Windows.Forms.Button btnEnviar;
+    }
+}
+
